f7eae94a15b157829d205957ac1b3b9fa53eca1f
Raj Kumar <raj.k.stats@gmail.com>
2022-03-21 01:07:05 +0530

    wk 3 update

--- week3/createContentTrainingData.py
+++ week3/createContentTrainingData.py
@@ -1,12 +1,26 @@
 import argparse
 import os
 import random
+import nltk
 import xml.etree.ElementTree as ET
 from pathlib import Path
+import pandas as pd
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.snowball import SnowballStemmer
 
 def transform_name(product_name):
-    # IMPLEMENT
-    return product_name
+
+    product_name_lower = product_name.lower()
+    
+    tokenizer = RegexpTokenizer(r'\w+')
+    tokens = tokenizer.tokenize(product_name_lower)
+    
+    snowball = SnowballStemmer("english")
+    tokens = [snowball.stem(token) for token in tokens]
+    new_product_name = " ".join(tokens)
+
+    return new_product_name
+
 
 # Directory for product data
 directory = r'/workspace/search_with_machine_learning_course/data/pruned_products/'
@@ -34,10 +48,15 @@ if args.input:
 # IMPLEMENT:  Track the number of items in each category and only output if above the min
 min_products = args.min_products
 sample_rate = args.sample_rate
+category_depth = 3
+
+print("Prcoessing products")
+print("CATEGORY DEPTH: {}".format(category_depth))
+products_list = []
 
 print("Writing results to %s" % output_file)
-with open(output_file, 'w') as output:
-    for filename in os.listdir(directory):
+
+for filename in os.listdir(directory):
         if filename.endswith(".xml"):
             print("Processing %s" % filename)
             f = os.path.join(directory, filename)
@@ -46,13 +65,26 @@ with open(output_file, 'w') as output:
             for child in root:
                 if random.random() > sample_rate:
                     continue
-                # Check to make sure category name is valid
                 if (child.find('name') is not None and child.find('name').text is not None and
                     child.find('categoryPath') is not None and len(child.find('categoryPath')) > 0 and
-                    child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text is not None):
+                    child.find('categoryPath')[len(child.find('categoryPath')) - category_depth][0].text is not None):
                       # Choose last element in categoryPath as the leaf categoryId
-                      cat = child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text
-                      # Replace newline chars with spaces so fastText doesn't complain
+                      cat = child.find('categoryPath')[len(child.find('categoryPath')) - category_depth][0].text
                       name = child.find('name').text.replace('\n', ' ')
-                      output.write("__label__%s %s\n" % (cat, transform_name(name)))
 
+                      product_dict = {}
+                      product_dict['category'] = cat
+                      product_dict['name'] = transform_name(name)
+                      products_list.append(product_dict)
+
+df = pd.DataFrame(products_list) 
+if min_products > 0:
+    print("Filtering min products per category results")
+    gb = df.groupby('category')
+    df_filtered = gb.filter(lambda x: x['category'].count() > min_products)
+    print("{} products filtered".format(len(df)-len(df_filtered)))
+    df = df_filtered
+    
+with open(output_file, 'w') as output:
+    for index, row in df.iterrows():
+        output.write("__label__%s %s\n" % (row['category'], row['name']))
